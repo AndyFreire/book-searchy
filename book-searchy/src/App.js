6266b2af6e3a4c1c00d07b41a6dbe1629d0c5913
Andrew Freire <andrew.m.freire@gmail.com>
2020-11-29 16:51:44 -0800

    Added Nav component

--- book-searchy/src/App.js
+++ book-searchy/src/App.js
@@ -1,11 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import API from "./utils/API";
+import Nav from "./components/Nav"
 
 function App(){
 
+  const [books, setBooks] = useState([]);
+  const [bookSearch, setBookSearch] = useState("");
+
+  const handleInputChange = event => {
+    // Destructure the name and value properties off of event.target
+    // Update the appropriate state
+    const { value } = event.target;
+    setBookSearch(value);
+  };
+
+  const handleFormSubmit = event => {
+    // When the form is submitted, prevent its default behavior, get recipes update the recipes state
+    event.preventDefault();
+    API.getBooks(bookSearch)
+      .then(res => setBooks(res.data))
+      .catch(err => console.log(err));
+  };
+
   return (
-    <div className="btn btn-primary">
-      Hello!
+    <div>
+      <Nav />
+      {/* <Jumbotron />
+      <Container>
+        <Row>
+          <Col size="md-12">
+            <form>
+              <Container>
+                <Row>
+                  <Col size="xs-9 sm-10">
+                    <Input
+                      name="RecipeSearch"
+                      value={recipeSearch}
+                      onChange={handleInputChange}
+                      placeholder="Search For a Recipe"
+                    />
+                  </Col>
+                  <Col size="xs-3 sm-2">
+                    <Button
+                      onClick={handleFormSubmit}
+                      type="success"
+                      className="input-lg"
+                    >
+                        Search
+                    </Button>
+                  </Col>
+                </Row>
+              </Container>
+            </form>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="xs-12">
+            {!recipes.length ? (
+              <h1 className="text-center">No Recipes to Display</h1>
+            ) : (
+              <RecipeList>
+                {recipes.map(recipe => {
+                  return (
+                    <RecipeListItem
+                      key={recipe.title}
+                      title={recipe.title}
+                      href={recipe.href}
+                      ingredients={recipe.ingredients}
+                      thumbnail={recipe.thumbnail}
+                    />
+                  );
+                })}
+              </RecipeList>
+            )}
+          </Col>
+        </Row>
+      </Container> */}
     </div>
   );
 
