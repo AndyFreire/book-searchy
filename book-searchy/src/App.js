e0115c9d7cfaa9ef2199c13d7c1770976eaa3d86
Andrew Freire <andrew.m.freire@gmail.com>
2020-12-01 20:14:50 -0800

    Added react-dom-router and fixed issues with missing thumbnails

--- book-searchy/src/App.js
+++ book-searchy/src/App.js
@@ -1,110 +1,34 @@
 import React, { useState } from "react";
-import API from "./utils/API";
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
 import Nav from "./components/Nav";
 import Jumbotron from "./components/Jumbotron";
-import Input from "./components/Input";
-import Button from "./components/Button";
-import { BookList, BookListItem } from "./components/BookList";
-import { Container, Row, Col } from "./components/Grid";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 require('dotenv').config()
 
 
-function App(){
-
-  const [books, setBooks] = useState([]);
-  const [bookSearch, setBookSearch] = useState("");
-
-  const handleInputChange = event => {
-    // Destructure the name and value properties off of event.target
-    // Update the appropriate state
-    const { value } = event.target;
-    setBookSearch(value);
-  };
-
-  const handleFormSubmit = event => {
-    // When the form is submitted, prevent its default behavior, get books update the books state
-    event.preventDefault();
-    API.searchBooks(bookSearch)
-      .then(res => {
-        console.log(res);
-        const searchedBooks = res.data.items.map(book => {
-          const title = book.volumeInfo.title;
-          const authors = book.volumeInfo.authors;
-          const description = book.volumeInfo.description || "";
-          const link = book.volumeInfo.infoLink;
-          const image = book.volumeInfo.imageLinks.thumbnail;
-          console.log(image);
-
-          return({
-            title: title,
-            authors: authors,
-            description: description,
-            link: link,
-            image: image
-          })
-        });
-        setBooks(searchedBooks);
-      })
-      .catch(err => console.log(err));
-  };
+function App() {
 
   return (
-    <div>
-      <Nav />
-      <Jumbotron />
-      <Container>
-        <Row>
-          <Col size="md-12">
-            <form>
-              <Container>
-                <Row>
-                  <Col size="xs-9 sm-10">
-                    <Input
-                      name="BookSearch"
-                      value={bookSearch}
-                      onChange={handleInputChange}
-                      placeholder="Search for Books"
-                    />
-                  </Col>
-                  <Col size="xs-3 sm-2">
-                    <Button
-                      onClick={handleFormSubmit}
-                      type="success"
-                      className="input-lg"
-                    >
-                        Search
-                    </Button>
-                  </Col>
-                </Row>
-              </Container>
-            </form>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="xs-12">
-            {!books.length ? (
-              <div>
-                <h5 className="text-xs-center text-muted w-100">No Books to Display</h5>
-              </div>
-            ) : (
-              <BookList>
-                {books.map(book => {
-                  return (
-                    <BookListItem
-                      key={book.title}
-                      title={book.title}
-                      link={book.link}
-                      authors={book.authors}
-                      image={book.image}
-                    />
-                  );
-                })}
-              </BookList>
-            )}
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Jumbotron />
+
+        <Switch>
+          <Route exact path={["/", "/search"]}>
+            <Search />
+          </Route>
+          {/* <Route exact path="/saved">
+            <Saved />
+          </Route> */}
+        </Switch>
+      </div>
+
+    </Router>
+
   );
 
 }
